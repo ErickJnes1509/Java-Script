894969a9cd5703d3139696515585c7df17aa75c7
ErickJnes1509 <68303051+ErickJnes1509@users.noreply.github.com>
2020-08-07 16:14:04 -0500

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,22 @@
+let num1 = 42;
+let str1 = "javascript is fun!!";
+let phrase = `carlos says ${str1}`;
+let phrase2 = `my fav number is ${num1}`;
+let carlosSmells = false;
+let fillThisLater;
+
+let age= 24;
+
+console.log(age);
+
+let favcolor = "purple;
+console.log(favColor);
+
+let favHorror = "It";
+comsole.log(favhorror);
+
+let favHero = "Superman;
+console.log(favHero);
+
+let tallerThanFiveFive = true;
+console.log(tallerThanFiveFive);
